31f2736dc8dcbcc6840fbac9407071ae2546d41c
huskov <p.huskov@gmail.com>
2015-04-29 14:53:59 +0300

    first commit

---
+++ app/src/main/java/androidhive/info/tcset/activity/DownloadsFragment.java
@@ -0,0 +1,31 @@
+package androidhive.info.tcset.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidhive.info.tcset.R;
+
+
+public class DownloadsFragment extends Fragment {
+
+
+    public DownloadsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_downloads, container, false);
+    }
+}
---
+++ app/src/main/java/androidhive/info/tcset/activity/ScheduleFragment.java
@@ -0,0 +1,31 @@
+package androidhive.info.tcset.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidhive.info.tcset.R;
+
+
+public class ScheduleFragment extends Fragment {
+
+
+    public ScheduleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+}
